58a56f75dd0594a8d9b340dc0e4d2e1a3bddb7a0
Haena <kimhn200605@gmail.com>
2022-02-23 19:11:00 +0900

    [Updated] 1158 : 요세푸스 문제

--- Algorithm/자료 구조/1158.py
+++ Algorithm/자료 구조/1158.py
@@ -1,5 +1,30 @@
 # 1158 : 요세푸스 문제
 
+# 방법 1) 데크 사용
+
+from collections import deque         # 데크 임포트
+
+n, k = map(int, input().split())      # n, k 입력받기
+deq = deque()           # 데크 생성
+
+result = []             # 요세푸스 순열 (출력할 값)
+point = k - 1           # 차례대로 삭제할 인덱스 번호
+
+for i in range(n) :     # 1번 ~ N 번 지정
+  deq.append(i+1)
+  
+while len(deq) != 0 :   # deq 배열 내부의 값이 존재할 때까지
+  deq.rotate(-point)    # -point 값만큼 왼쪽으로 회전
+  result.append(str(deq.popleft()))     # deq 배열의 맨 왼쪽 요소를 문자열로 반환한 후 삭제
+  
+# 출력 형태 지정
+# 문자열 메서드 join 을 사용하기 위해 삭제할 요소를 문자열로 변환했던 것
+print("<", ", ".join(result), ">", sep='')
+
+# -----------------------------------------------------------------------
+
+# 방법 2
+
 n, k = map(int, input().split())        # n, k 입력받기
 arr = [0] * n           # n 개의 원소를 가진 배열 만들기
 result = []             # 요세푸스 순열 (출력할 값)
