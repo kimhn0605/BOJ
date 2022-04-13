3ae1c7ff8b47134aaee98738f1221a7d7744e897
Haena <kimhn200605@gmail.com>
2022-04-13 22:47:54 +0900

    [Updated] 11866 : 요세푸스 문제 0

--- Algorithm/자료 구조/11866.py
+++ Algorithm/자료 구조/11866.py
@@ -1,5 +1,26 @@
 # 11866 : 요세푸스 문제 0
 
+# 방법 1
+
+n, k = map(int, input().split())      # n, k 입력받기
+li = list(range(1, n+1))              # 1번 ~ n번까지의 리스트 생성
+print("<", end="")
+i = 0                             
+
+while len(li) > 1 :     # 리스트 요소가 2개 이상일 동안 반복
+  i += k
+  
+  if i > len(li) :      # 인덱스 i 가 리스트 크기보다 크다면
+    i %= len(li)        # 리스트 크기로 나눈 나머지만큼 인덱스 감소
+    if i == 0 :         # 리스트 크기로 나눴을 때 딱 나누어떨어졌다면
+      i += len(li)      # 다시 리스트 크기만큼 증가 (어차피 아래에서 -1하니까 범위 초과 x)
+      
+  i -= 1             
+  print(str(li.pop(i)), end=", ")       # 리스트에서 i 번째 인덱스 제거
+  
+print("{}>".format(str(li.pop())))      # 마지막 남은 원소 제거하면서 출력
+  
+# 방법 2
 n, k = map(int, input().split())      # n, k 입력받기
 li = list(range(1, n+1))              # 1번 ~ n번까지의 리스트 생성
 i = k-1                               # 리스트에서 삭제할 인덱스 번호 (k-1)
