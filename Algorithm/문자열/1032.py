a10b7ac69330b38bf0b77d99f14034ae11a0686c
Haena <kimhn200605@gmail.com>
2022-04-25 18:56:08 +0900

    [Updated] 1032 : 명령 프롬프트

--- Algorithm/문자열/1032.py
+++ Algorithm/문자열/1032.py
@@ -1,5 +1,6 @@
 # 1032 : 명령 프롬프트
 
+# 방법 1)
 n = int(input())                    # n 입력받기
 li = []
 
@@ -14,3 +15,24 @@ for word in li[1:] :                # 두 번째 단어부터 끝까지 순회
       result = result[:i] + "?" + result[i+1:]
       
 print(result)
+
+# 방법 2)
+n = int(input())                    # n 입력받기
+
+front = list(input())               # 처음 입력받은 단어만 따로 리스트에 저장
+length = len(front)                 # length : 단어의 길이 저장
+
+for i in range(n-1) :               # 나머지 단어의 입력이 들어올 때마다
+  back = list(input())
+  
+  for j in range(length) :          # 단어의 길이만큼 순회하면서 알파벳 비교
+    if front[j] != back[j] :
+      front[j] = "?"
+
+# print(front)
+print("".join(front))               # join() : 문자열 합치는 메서드
+
+# ex) abc / bac / cbc
+
+# print(front) => ['?', '?', 'c']
+# print("".join(front)) => ??c
