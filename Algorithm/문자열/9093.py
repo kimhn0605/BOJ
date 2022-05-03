1f34c119c5bc0392e4a9441cae6d7e84c045635f
Haena <kimhn200605@gmail.com>
2022-05-04 00:53:18 +0900

    [Updated] 9093 : 단어 뒤집기

--- Algorithm/문자열/9093.py
+++ Algorithm/문자열/9093.py
@@ -1,5 +1,6 @@
 # 9093 : 단어 뒤집기
 
+# 방법 1) 문자열 슬라이싱 사용
 t = int(input())                    # 테스트 케이스 개수 입력받기
 
 for _ in range(t) :                 
@@ -7,4 +8,14 @@ for _ in range(t) :
   
   for w in words :                  # 단어 하나씩 순회하면서
     print(w[::-1], end=" ")         # 슬라이싱 이용해서 문자열 뒤집기 
+  print()
+
+# 방법 2) reversed() 메서드 사용
+t = int(input())
+
+for _ in range(t) :
+  words = list(input().split())
+  for w in words :
+    # reversed() : reversed object 반환하므로 join 함수 이용해서 문자열 반환
+    print(''.join(reversed(w)), end= " ")     
   print()
