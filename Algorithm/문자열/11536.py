07de584339981ed56a4a53ad3060ced18e7659ae
Haena <kimhn200605@gmail.com>
2022-05-06 17:18:23 +0900

    [Updated] 11536 : 줄 세우기

--- Algorithm/문자열/11536.py
+++ Algorithm/문자열/11536.py
@@ -1,5 +1,6 @@
 # 11536 : 줄 세우기
 
+# 방법 1) 정렬 이용
 n = int(input())                # n 입력받기
 
 result = 'NEITHER'              # 출력할 문구 초기화
@@ -36,3 +37,21 @@ for i in range(n) :
     break
     
 print(result)
+
+# 방법 2) 불리언값 이용
+n = int(input())                            # n 입력받기
+name = [input() for _ in range(n)]          # 리스트에 입력받은 이름 저장
+
+# check 초깃값이 참이라면 오름차순
+check = (name[0] < name[1])
+
+for i in range(1, n-1) :                    # 리스트 요소 하나씩 돌아가면서 비교
+  if check != (name[i] < name[i+1]) :       # 비교값이 기존 check 값과 동일하지 않다면 
+    print("NEITHER")                        # NEITHER 출력 후 프로그램 종료
+    quit()                                  
+    
+if check :                                  # check 값이 True 라면 INCREASING 출력
+  print("INCREASING")
+else :                                      # check 값이 False 라면 DECREASING 출력
+  print("DECREASING")
+
