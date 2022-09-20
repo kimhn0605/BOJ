07ead40afec950cd0c9744a52f4eb6fcc5eef4ee
Haena <kimhn200605@gmail.com>
2022-09-21 04:04:29 +0900

    [Solved] 15652 : N과 M (4)

---
+++ Algorithm/백트래킹/15652.py
@@ -0,0 +1,10 @@
+# 15652 : N과 M (4)
+
+from itertools import combinations_with_replacement     # 중복 조합 라이브러리
+
+n, m = map(int, input().split())                        # n, m 입력받기
+li = [i for i in range(1, n+1)]                         # 1 ~ n 까지의 자연수 집합
+
+for comb in combinations_with_replacement(li, m) :      # 중복 조합 결과를 하나씩 순회하면서
+  comb = str(comb)                                      # 문자열 변환 후 
+  print(''.join(comb.split(","))[1:-1])                 # 형식에 맞게 출력
